8058cd754e3c70cc5d6758f2accbdb3536d96b5d
gyu-don <takumi.kt@gmail.com>
2024-12-22 04:29:32 +0900

    badThread

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module model-checker
+module main
 
 go 1.23.4
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	sys := Mutex()
+	model, err := KripkeModel(sys)
+	if err != nil {
+		log.Fatal(err)
+	}
+	model.WriteAsDot(os.Stdout)
 }
